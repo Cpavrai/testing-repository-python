47cf2120b2c892f67a97848eb33520cf8215e046
Charles Parmentier <32079518+Cpavrai@users.noreply.github.com>
2024-11-28 19:45:08 +0100

    Updated core

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import requests
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def fetch_ip_address():
-  data = requests.get("https://ifconfig.me")
+  data = requests.get(os.environ['IF_CONFIG_URL'])
   print(f"Your router IP address is : {data.text}")
 
 def getPictureOfTheDay():
-    data = requests.get(f"https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-    print("Votre image du jour est disponible :", data.json()['url'])
+    data = requests.get(f"{os.environ['APOD_URL']}?api_key={os.environ.get('APOD_KEY', 'DEMO_KEY')}")
+    print(
+      "Votre image du jour est disponible :", data.json()['url'])
 
 fetch_ip_address()
 getPictureOfTheDay()
